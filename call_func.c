d29760e72ad6ccadfefe26b71d8c30c7186ee1e2
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-10-06 12:51:05 +0100

    finds the right function to execute

--- call_func.c
+++ call_func.c
@@ -1,3 +1,26 @@
 #include "monty.h"
+/**
+ * call_func - searches and finds the matching function
+ * @search: the line gotten from the bytecode serving as the
+ * instructions
+ *
+ * Return: pointer to function that matches. NULL if otherwise
+ */
 
+void (*call_func(char **search))(stack_t **, unsigned int)
+{
+	instruction_t instructions_arr[] = {
+		{"push", push_handler},
+		{"pall", pall_handler},
+		{NULL, NULL}
+	};
+	int op_codes = 2, i;
 
+	for (i = 0; i < 2; i++)
+	{
+		if (strcmp(intstuctions_arr[i].opcode, search[0]) == 0)
+			return (instructions_arr[i].f);
+	}
+
+	return (NULL);
+}
